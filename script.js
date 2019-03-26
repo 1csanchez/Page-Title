ea3f8c33f36c9e8c13b055b5857a5f99e5163074
1csanchez <45851396+1csanchez@users.noreply.github.com>
2019-03-26 08:34:53 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,22 @@
+var a;
+var b;
+var c;
+var d;
+
+$("button").click(function(){
+    a=$(".ad").val();
+     console.log(a);
+    $(".sd").html(a);
+    
+    b=$(".nod").val();
+     console.log(b);
+    $(".fd").html(b);
+    
+    c=$(".id").val();
+     console.log(c);
+    $(".lo").html(c);
+    
+    d=$(".sf").val();
+     console.log(d);
+    $(".kf").html(d);
+});
